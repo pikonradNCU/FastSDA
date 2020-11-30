02bd3f538ddc6b553459965e4b8640ded8a5efa1
pikonrad <pikonrad@gmail.com>
2020-11-30 17:17:48 +0100

    begin maven project

---
+++ src/main/java/pl/umk/mat/CommandLineApp.java
@@ -0,0 +1,13 @@
+package pl.umk.mat;
+
+/**
+ * Hello world!
+ *
+ */
+public class CommandLineApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
